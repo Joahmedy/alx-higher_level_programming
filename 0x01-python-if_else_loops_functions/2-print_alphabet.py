4bb33ac14b0e0c25f4ac02271f4147110f821896
Youssef jo <joahmedy777@gmail.com>
2023-11-29 03:42:49 +0200

    jo

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for letter in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(letter), end='')
