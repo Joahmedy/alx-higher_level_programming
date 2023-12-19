89fd91af091aeab66ecaef2150d64db1d8c48d03
Youssef jo <joahmedy777@gmail.com>
2023-12-19 03:37:42 +0200

    jo

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-def safe_print_list_integers(my_list=[], x=0):
-    num = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            num += 1
-        except (ValueError, TypeError):
-            continue
-    print("")
-    return (num)
+
+def safe_print_division(a, b):
+    """
+    divides two integers and prints the result
+    catches divide by zero exception
+    """
+    try:
+        res = a / b
+    except:
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return res
